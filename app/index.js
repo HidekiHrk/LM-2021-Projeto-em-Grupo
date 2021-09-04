ecd6d9ea8058e423bff3910d273ee408c4fb53ad
HidekiHrk <hidekihiroki123@gmail.com>
2021-09-04 01:23:21 -0300

    feat: controls created

--- app/index.js
+++ app/index.js
@@ -1,3 +1,12 @@
+const setProgress = (element, value) => {
+  element.style.background = `linear-gradient(to right, var(--color-light-gray) ${value}%, var(--color-block-border) ${value}%)`;
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("HI!");
+  const progressBar = document.getElementById("progress-bar");
+  setProgress(progressBar, progressBar.value);
+  progressBar.addEventListener("input", (e) => {
+    const value = e.target.value;
+    setProgress(e.target, value);
+  });
 });
